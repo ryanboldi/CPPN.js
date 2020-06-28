a98817373fe5c522eb950ed83328b726e040c0bf
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-28 12:10:45 +0400

    IMAGE GENERATION WORKS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+let width = 520;
+let height = 520;
+
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height);
+
+    let img = createImage(width, height);
+    img.loadPixels();
+    let g = new Genome(2, 3);
+
+    for (let i = 0; i < img.width; i++) {
+        for (let j = 0; j < img.height; j++) {
+            let outputs = g.feedforward([i, j]);
+            img.set(i, j, color(outputs[0], outputs[1], outputs[2]))
+        }
+    }
+
+    img.updatePixels();
+    image(img, 0, 0);
 }
 
 function draw() {
