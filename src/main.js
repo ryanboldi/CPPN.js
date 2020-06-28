3f2aa4228e7413506f864e33661001101d1e4ea4
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-28 12:24:24 +0400

    replaced with 512x512 images

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-let width = 520;
-let height = 520;
+let width = 512;
+let height = 512;
 
 let img;
+let g;
 
 function setup() {
     createCanvas(width, height);
 
-    img = createImage(width, height)
+    img = createImage(width, height);
 
     img.loadPixels();
-    let g = new Genome(2, 3);
+    g = new Genome(2, 3);
+
+    //for (let i = 0; i< 100; i++){
+     //   g.mu_add_node();
+    //}
 
     for (let i = 0; i < img.width; i++) {
         for (let j = 0; j < img.height; j++) {
