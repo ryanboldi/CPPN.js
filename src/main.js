e2a114e8d74f75a067d1f85f79eae15ec55496b1
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-29 12:02:43 +0400

    mutation works, images still don't have shapes

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ let height = Math.sqrt(photoAmt) * photoHeight;
 
 let photos = [];
 
+let selected = new Array(photoAmt).fill(0);
 
 function setup() {
     createCanvas(width, height);
@@ -21,7 +22,7 @@ function setup() {
 
 function draw() {
     drawPhoto();
-    
+
     //g.Mutate();
 
 
@@ -39,7 +40,7 @@ function save() {
 }
 
 function drawPhoto() {
-    
+
     //for (let i = 0; i< 100; i++){
     //   g.mu_add_node();
     //}
@@ -49,41 +50,85 @@ function drawPhoto() {
     for (let k = 0; k < photos.length; k++) {
         img = createImage(photoWidth, photoHeight);
         img.loadPixels();
+
         for (let i = 0; i < img.width; i++) {
             for (let j = 0; j < img.height; j++) {
-                let outputs = photos[k].feedforward([i / photoWidth, j / photoHeight, distFromCenter(i/photoWidth, j/photoHeight)]); //, Math.sqrt((i/width)**2 + (j/height)**2)
+                let outputs = photos[k].feedforward([i / photoWidth, j / photoHeight, distFromCenter(i / photoWidth, j / photoHeight)]); //, Math.sqrt((i/width)**2 + (j/height)**2)
                 //img.set(i, j, color(Math.abs(outputs[0]), Math.abs(outputs[1]), Math.abs(outputs[2])));
-                img.set(i, j, color(outputs[0], outputs[1], outputs[2]));
+                img.set(i, j, color(Math.abs(outputs[0]), Math.abs(outputs[1]), Math.abs(outputs[2])));
             }
         }
         img.updatePixels();
         image(img, x, y);
+        if (selected[k] == 1) {
+            stroke(0, 1, 0);
+            strokeWeight(3);
+            noFill();
+            rect(x, y, photoWidth, photoHeight);
+        }
         x += photoWidth;
-        if (x == width){
-            x= 0;
+        if (x == width) {
+            x = 0;
             y += photoWidth;
         }
+
+
     }
-    
+
 }
 
-function cross(parent1, parent2){
+function cross(parent1, parent2) {
     let p1 = photos[parent1];
     let p2 = photos[parent2];
-    for (let i = 0; i < photos.length; i++){
-        photos[i] = crossover(p1,p2);
+    for (let i = 0; i < photos.length; i++) {
+        photos[i] = crossover(p1, p2);
         photos[i].Mutate();
-     }
+    }
 }
 
 //X AND Y BOTH OUT OF 1
-function distFromCenter(x,y){
-    return(Math.sqrt(Math.abs(x - 1/2)**2 + Math.abs(y - 1/2)**2));
+function distFromCenter(x, y) {
+    return (Math.sqrt(Math.abs(x - 1 / 2) ** 2 + Math.abs(y - 1 / 2) ** 2));
 }
 
-function mousePressed(){
-    console.log(`clicked at ${Math.floor(mouseX/photoWidth)}, ${Math.floor(mouseY/photoHeight)}`);
+function mousePressed() {
+    //console.log(`clicked at ${Math.floor(mouseX / photoWidth)}, ${Math.floor(mouseY / photoHeight)}`);
 
-    let index = ((Math.floor(mouseY/photoHeight)) * Math.sqrt(photoAmt)) + Math.floor(mouseX/photoWidth);
-    console.log(index);
+    let index = ((Math.floor(mouseY / photoHeight)) * Math.sqrt(photoAmt)) + Math.floor(mouseX / photoWidth);
+    //console.log(index);
+
+    if (index >= 0 && index <= photoAmt) {
+        if (selected[index] == 1) selected[index] = 0;
+        else if (selected[index] == 0) selected[index] = 1;
+    }
+
+    //console.log(selected);
+}
+
+function keyPressed() {
+    if (keyCode == 32) {
+        let parents = []
+        for (let i = 0; i < photos.length; i++){
+            if (selected[i] == 1){
+                parents.push(photos[i].clone())
+            }
+        }
+        crossMult(parents);
+        selected = new Array(photoAmt).fill(0);
+    }
+}
+
+function crossMult(parents) {
+    for (let k = 0; k < photos.length; k++) {
+        dance:
+        for (let i = 0; i < parents.length; i++) {
+            for (let j = 0; j < parents.length; j++) {
+                if (Math.random() < 1 / (parents.length)) {
+                    photos[k] = crossover(parents[i], parents[j]);
+                    photos[k].Mutate();
+                    break dance; //har har
+                }
+            }
+        }
+    }
 }
